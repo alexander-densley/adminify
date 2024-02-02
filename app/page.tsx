11284c86d64748a10364a9bb6d2164023221affc
Alexander Densley <densley.alexander@gmail.com>
2024-02-01 18:24:11 -0700

    button

--- app/page.tsx
+++ app/page.tsx
@@ -1,88 +1,89 @@
 'use client'
+import { CustomButton } from '@/components/custom-button'
 import {
 	Tabs,
 	Tab,
 	Input,
 	Link,
-	Button,
 	Card,
 	CardBody,
 	CardHeader,
 } from '@nextui-org/react'
-import { useState } from 'react'
+import { useRef } from 'react'
+import { Button } from '@nextui-org/react'
 export default function App() {
+	const buttonRef = useRef<HTMLButtonElement | null>(null)
+
+	const handleConfetti = async () => {
+		const { clientWidth, clientHeight } = document.documentElement
+		const boundingBox = buttonRef.current?.getBoundingClientRect?.()
+
+		const targetY = boundingBox?.y ?? 0
+		const targetX = boundingBox?.x ?? 0
+		const targetWidth = boundingBox?.width ?? 0
+
+		const targetCenterX = targetX + targetWidth / 2
+		const confetti = (await import('canvas-confetti')).default
+
+		confetti({
+			zIndex: 999,
+			particleCount: 100,
+			spread: 70,
+			origin: {
+				y: targetY / clientHeight,
+				x: targetCenterX / clientWidth,
+			},
+		})
+	}
 	return (
 		<div>
-			<p className='text-5xl my-10 text-center'>Adminify backend form</p>
+			<p className='text-5xl my-10 text-center'>Adminify Backend Form</p>
 			<div className='flex flex-col w-full items-center'>
 				<Card className='max-w-full w-[500px]'>
 					<CardBody className='overflow-hidden'>
 						<form className='flex flex-col gap-4'>
-							<Input
-								isRequired
-								label='Name'
-								placeholder='Enter company name'
-								type='text'
-							/>
-							<Input
-								isRequired
-								label='openAi Api Key'
-								placeholder='Enter your openAi api key'
-								type='text'
-							/>
-							<Input
-								isRequired
-								label='openAi Prompt'
-								placeholder='Enter your openAi prompt'
-								type='text'
-							/>
+							<Input isRequired label='Name' type='text' />
+							<Input isRequired label='openAi Api Key' type='text' />
+							<Input isRequired label='openAi Prompt' type='text' />
 							<Input
 								isRequired
 								label='Go High Level Access Token'
-								placeholder='Enter your go high level access token'
 								type='text'
 							/>
 							<Input
 								isRequired
 								label='Go High Level Refresh Token'
-								placeholder='Enter your go high level refresh token'
 								type='text'
 							/>
-							<Input
-								isRequired
-								label='Go High Level Client ID'
-								placeholder='Enter your go high level client id'
-								type='text'
-							/>
+							<Input isRequired label='Go High Level Client ID' type='text' />
 							<Input
 								isRequired
 								label='Go High Level Client Secret'
-								placeholder='Enter your go high level client secret'
 								type='text'
 							/>
-							<Input
-								isRequired
-								label='Go High Level Location ID'
-								placeholder='Enter your go high level location id'
-								type='text'
-							/>
+							<Input isRequired label='Go High Level Location ID' type='text' />
 							<Input
 								isRequired
 								label='Ai Recent Message Count Threshold'
-								placeholder='Enter your ai recent message count threshold'
 								type='text'
 							/>
 							<Input
 								isRequired
 								label='Timezone'
-								placeholder='Enter your timezone'
 								// TODO: Add type
-
 								type='text'
 							/>
 
 							<div className='flex gap-2 justify-end'>
-								<Button fullWidth color='primary'>
+								<Button
+									color='primary'
+									fullWidth
+									ref={buttonRef}
+									disableRipple
+									className="relative overflow-visible rounded-full hover:-translate-y-1 px-12 shadow-xl  after:content-[''] after:absolute after:rounded-full after:inset-0 after:bg-background/40 after:z-[-1] after:transition after:!duration-500 hover:after:scale-150 hover:after:opacity-0"
+									size='lg'
+									onPress={handleConfetti}
+								>
 									Add company to database
 								</Button>
 							</div>
@@ -90,6 +91,7 @@ export default function App() {
 					</CardBody>
 				</Card>
 			</div>
+			<CustomButton />
 		</div>
 	)
 }
